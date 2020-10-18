b43aec371aa5061cad97d580e8529b80a32629b8
“Enter <merin@gmail.com>
2020-10-18 23:31:25 +0530

    commit

--- solution.sh
+++ solution.sh
@@ -76,6 +76,28 @@ echo "$compute"
 
 
 
+#! /bin/bash -x
+
+
+
+echo "enter number1" a
+read a
+
+echo "enter number2" b
+read b
+
+echo "enter number3" c
+read c
+
+
+compute=$(( $a % $b  + $c ))
+echo "$compute"
+
+
+
+
+
+
 
 
 
