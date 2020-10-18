32bb19bc3c12ffb7c5f19ae8609c0a28bd05a9ab
“Enter <merin@gmail.com>
2020-10-18 23:38:48 +0530

    seventh commit

--- solution.sh
+++ solution.sh
@@ -123,6 +123,47 @@ echo "${computation[@]}"
 
 
 
+#! /bin/bash
+
+declare -A computation
+declare -A array
+echo "enter number1" a
+read a
+
+echo "enter number2" b
+read b
+
+echo "enter number3" c
+read c
+
+computation[compute1]=$(( $a + $b * $c ))
+
+
+computation[compute2]=$(( $a * $b + $c ))
+
+computation[compute3]=$(( $c + $a  / $b ))
+
+computation[compute4]=$(( $a % $b  / $c ))
+
+
+
+
+value1=${computation[compute1]}
+ value2=${computation[compute2]}
+
+ value3=${computation[compute3]}
+
+value4=${computation[compute4]}
+
+array[index0]="$value1"
+array[index1]="$value2"
+array[index2]="$value3"
+array[index3]="$value4"
+
+
+
+echo ${array[@]}
+
 
 
 
