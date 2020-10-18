b9b3eb820d014f0ef5075c087231602ffc8487e3
“Enter <merin@gmail.com>
2020-10-18 23:43:21 +0530

    eighth commit

--- solution.sh
+++ solution.sh
@@ -168,6 +168,53 @@ echo ${array[@]}
 
 
 
+#! /bin/bash
+
+declare -A computation
+declare -A array
+echo "enter number1" a
+read a
+
+echo "enter number2" b
+read b
+
+echo "enter number3" c
+read c
+
+computation[compute1]=$(( $a + $b * $c ))
+
+
+computation[compute2]=$(( $a * $b + $c ))
+
+computation[compute3]=$(( $c + $a  / $b ))
+
+computation[compute4]=$(( $a % $b  / $c ))
+
+
+value1=${computation[compute1]}
+ value2=${computation[compute2]}
+
+ value3=${computation[compute3]}
+
+value4=${computation[compute4]}
+
+array[index0]="$value1"
+array[index1]="$value2"
+array[index2]="$value3"
+array[index3]="$value4"
+
+echo ${array[@]}
+for el in "${array[@]}"
+do
+        echo "$el"
+done | sort  -nr
+array1=( $(
+      for el in ${array[@]}
+      do
+        echo $el
+      done | sort -nr) )
+
+echo ${array1[@]}
 
 
 
