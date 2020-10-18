268535418129614cf2fb296bf72da20c1bd3c249
“Enter <merin@gmail.com>
2020-10-18 23:06:55 +0530

    my first commit

--- solution.sh
+++ solution.sh
@@ -1,16 +1,6 @@
 WELCOME OF THE PROGRAM BEING SOLVED IN MASTER BRANCH
 
 
-#! /bin/bash -x
-
-echo "enter number1" a
-read a
-
-echo "enter number2" b
-read b
-
-echo "enter number3" c
-read c
 
 
 
