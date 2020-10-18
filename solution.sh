02b70454fad8c735f50093d15564bcb90a554bcc
“Enter <merin@gmail.com>
2020-10-18 23:19:19 +0530

    third commit

--- solution.sh
+++ solution.sh
@@ -2,10 +2,36 @@ WELCOME OF THE PROGRAM BEING SOLVED IN MASTER BRANCH
 
 
 
+#! /bin/bash
+
+echo "enter number1" a
+read a
+
+echo "enter number2" b
+read b
+
+echo "enter number3" c
+read c
 
 
 
 
+
+
+#! /bin/bash
+
+echo "enter number1" a
+read a
+
+echo "enter number2" b
+read b
+
+echo "enter number3" c
+read c
+
+
+compute=$(( $a + $b * $c ))
+echo "$compute"
 
 
 
